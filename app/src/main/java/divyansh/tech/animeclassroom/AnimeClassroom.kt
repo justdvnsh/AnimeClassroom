72644c6dcdd9d83b021fd420f378c6b207bfa655
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2021-04-26 18:04:20 +0530

    added multi-modular architecture

---
+++ app/src/main/java/divyansh/tech/animeclassroom/AnimeClassroom.kt
@@ -0,0 +1,7 @@
+package divyansh.tech.animeclassroom
+
+import android.app.Application
+
+class AnimeClassroom: Application() {
+    override fun onCreate() = super.onCreate()
+}
---
+++ app/src/main/java/divyansh/tech/animeclassroom/di/ActivityModule.kt
@@ -0,0 +1,4 @@
+package divyansh.tech.animeclassroom.di
+
+class ActivityModule {
+}
