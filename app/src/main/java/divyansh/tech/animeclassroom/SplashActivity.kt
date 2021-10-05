8e276dfd44727039bcab1a4f00dd5abd95e8e10c
PranavVyas20 <Pranavvyas2003@gmail.com>
2021-10-05 16:47:53 +0530

    Animated splash screen

---
+++ app/src/main/java/divyansh/tech/animeclassroom/SplashActivity.kt
@@ -0,0 +1,25 @@
+package divyansh.tech.animeclassroom
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
