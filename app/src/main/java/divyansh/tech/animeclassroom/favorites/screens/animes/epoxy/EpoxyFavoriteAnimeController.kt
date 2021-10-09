b6ce18e8b83c724ce22bba82f8506c84f0319ea7
pauliancu97 <paul97iancu@gmail.com>
2021-10-09 20:52:59 +0300

    fully implemented the favorites anime page

---
+++ app/src/main/java/divyansh/tech/animeclassroom/favorites/screens/animes/epoxy/EpoxyFavoriteAnimeController.kt
@@ -0,0 +1,22 @@
+package divyansh.tech.animeclassroom.favorites.screens.animes.epoxy
+
+import com.airbnb.epoxy.TypedEpoxyController
+import divyansh.tech.animeclassroom.common.AnimeClickCallback
+import divyansh.tech.animeclassroom.models.home.OfflineAnimeModel
+
+class EpoxyFavoriteAnimeController(
+    private val animeClickCallback: AnimeClickCallback
+) : TypedEpoxyController<List<OfflineAnimeModel>>() {
+
+    override fun buildModels(data: List<OfflineAnimeModel>?) {
+        data?.let { offlineAnimeModels ->
+            offlineAnimeModels.forEach {
+                epoxyFavoriteAnime {
+                    id(it.animeUrl)
+                    anime(it)
+                    animeCallback(animeClickCallback)
+                }
+            }
+        }
+    }
+}
